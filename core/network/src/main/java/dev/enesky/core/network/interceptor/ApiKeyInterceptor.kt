143e027aed6115c88bf297d4f168b65d20701636
Enes Kamil Yılmaz <eneskamilyilmaz@gmail.com>
2025-02-22 08:47:50 +0300

    add: network calls with retrofit

---
+++ core/network/src/main/java/dev/enesky/core/network/interceptor/ApiKeyInterceptor.kt
@@ -0,0 +1,26 @@
+package dev.enesky.core.network.interceptor
+
+import dev.enesky.core.network.BuildConfig
+import dev.enesky.core.network.api.ApiEndpoints
+import okhttp3.Interceptor
+
+/**
+ * Interceptor for adding api key and language to the request
+ */
+class ApiKeyInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+        val original = chain.request()
+        val originalHttpUrl = original.url
+
+        val url = originalHttpUrl.newBuilder()
+            .addQueryParameter(ApiEndpoints.Query.API_KEY, BuildConfig.MOVIE_API_KEY)
+            .addQueryParameter(ApiEndpoints.Query.LANGUAGE, ApiEndpoints.DefaultValues.DEFAULT_LANGUAGE)
+            .build()
+
+        val request = original.newBuilder()
+            .url(url)
+            .build()
+
+        return chain.proceed(request)
+    }
+}
