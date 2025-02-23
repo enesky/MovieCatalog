d1dbefa96d307d7eb075523d4c1c62cfc15c7dd5
Enes Kamil Yılmaz <eneskamilyilmaz@gmail.com>
2025-02-23 09:52:26 +0300

    update: GetMovieDetailsUseCase with mapper usage

--- core/domain/src/main/java/dev/enesky/core/domain/usecase/GetMovieDetailsUseCase.kt
+++ core/domain/src/main/java/dev/enesky/core/domain/usecase/GetMovieDetailsUseCase.kt
@@ -1,20 +1,21 @@
 package dev.enesky.core.domain.usecase
 
 import dev.enesky.core.common.data.Resource
-import dev.enesky.core.data.model.MovieDetailDto
+import dev.enesky.core.common.data.map
+import dev.enesky.core.domain.mapper.MovieDetailMapper
+import dev.enesky.core.domain.model.MovieDetail
 import dev.enesky.core.network.repository.MovieRepository
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 
 /**
  * Created by Enes Kamil YILMAZ on 22/02/2025
  */
 
-class GetMovieDetailsUseCase(private val movieRepository: MovieRepository) {
-    operator fun invoke(id: Int): Flow<Resource<MovieDetailDto>> {
-        return flow {
-            val result = movieRepository.getMovieDetails(id)
-            emit(result)
+class GetMovieDetailsUseCase(
+    private val movieRepository: MovieRepository,
+    private val movieDetailMapper: MovieDetailMapper
+) {
+    suspend operator fun invoke(id: Int): Resource<MovieDetail> =
+        movieRepository.getMovieDetails(id).map {
+            movieDetailMapper.mapToDomain(it)
         }
-    }
 }
