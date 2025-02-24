c2fe49c2f4d029d4806aca635e1ba2bfa4d998e9
Enes Kamil Yılmaz <eneskamilyilmaz@gmail.com>
2025-02-25 00:21:52 +0300

    add: theme ant its related classes

---
+++ core/ui/src/main/java/dev/enesky/core/ui/theme/Spacing.kt
@@ -0,0 +1,41 @@
+package dev.enesky.core.ui.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+/**
+ * Created by Enes Kamil YILMAZ on 25/02/2025
+ */
+
+private val DefaultSpace = 0.dp
+private val BorderStroke = 1.dp
+private val XXXS = 2.dp
+private val XXS = 4.dp
+private val XS = 8.dp
+private val S = 12.dp
+private val M = 16.dp
+private val L = 24.dp
+private val XL = 32.dp
+private val XXL = 40.dp
+private val XXXL = 64.dp
+
+@Immutable
+data class MovieCatalogSpacing(
+    val default: Dp = DefaultSpace,
+    val border: Dp = BorderStroke,
+    val statusBarPadding: Dp = XXL,
+
+    val xxxSmall: Dp = XXXS,
+    val xxSmall: Dp = XXS,
+    val xSmall: Dp = XS,
+    val small: Dp = S,
+    val medium: Dp = M,
+    val large: Dp = L,
+    val xLarge: Dp = XL,
+    val xxLarge: Dp = XXL,
+    val xxxLarge: Dp = XXXL,
+)
+
+internal val LocalMovieCatalogSpacing = staticCompositionLocalOf { MovieCatalogSpacing() }
--- core/ui/src/main/java/dev/enesky/core/ui/theme/Theme.kt
+++ core/ui/src/main/java/dev/enesky/core/ui/theme/Theme.kt
@@ -3,39 +3,15 @@ package dev.enesky.core.ui.theme
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.platform.LocalContext
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
 
 @Composable
 fun MovieCatalogTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -52,6 +28,19 @@ fun MovieCatalogTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
+}
+
+object MovieCatalogTheme {
+    val shapes: MovieCatalogShapes
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalMovieCatalogShapes.current
+
+    val spacing: MovieCatalogSpacing
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalMovieCatalogSpacing.current
 }
