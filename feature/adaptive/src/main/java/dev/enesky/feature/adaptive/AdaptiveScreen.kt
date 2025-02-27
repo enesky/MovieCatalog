53f6e5003d65ee496d4175b35b7ec34d8580060a
Enes Kamil Yılmaz <eneskamilyilmaz@gmail.com>
2025-02-27 12:02:37 +0300

    add: enhance adaptive layout with home and detail screens integration

--- feature/adaptive/src/main/java/dev/enesky/feature/adaptive/AdaptiveScreen.kt
+++ feature/adaptive/src/main/java/dev/enesky/feature/adaptive/AdaptiveScreen.kt
@@ -1,19 +1,32 @@
 package dev.enesky.feature.adaptive
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.WindowAdaptiveInfo
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.material3.adaptive.layout.AnimatedPane
+import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffold
+import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldRole
+import androidx.compose.material3.adaptive.layout.ThreePaneScaffoldDestinationItem
+import androidx.compose.material3.adaptive.layout.calculatePaneScaffoldDirective
+import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dev.enesky.core.common.utils.ObserveAsEvents
-import dev.enesky.core.domain.constant.MovieConstants
 import dev.enesky.core.domain.model.MovieDetail
-import dev.enesky.core.ui.annotation.PreviewUiMode
-import dev.enesky.core.ui.theme.MovieCatalogTheme
+import dev.enesky.feature.detail.DetailScreen
+import dev.enesky.feature.detail.DetailViewModel
+import dev.enesky.feature.home.HomeScreen
+import dev.enesky.feature.home.HomeViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
 
@@ -23,42 +36,84 @@ import kotlinx.coroutines.flow.emptyFlow
 @Composable
 fun AdaptiveScreen(
     modifier: Modifier = Modifier,
-    uiState: PlayerUiState = PlayerUiState(),
-    eventFlow: Flow<PlayerEvent> = emptyFlow(),
+    uiState: AdaptiveUiState = AdaptiveUiState(),
+    homeViewModel: HomeViewModel = hiltViewModel(),
+    detailViewModel: DetailViewModel = hiltViewModel(),
+    eventFlow: Flow<AdaptiveEvent> = emptyFlow(),
 ) {
-    ObserveAsEvents(eventFlow) { playerEvent ->
-        when (playerEvent) {
-            is PlayerEvent.OnError -> { /* Handle error */ }
+    val detailUiState by detailViewModel.uiState.collectAsStateWithLifecycle()
+    val homeUiState by homeViewModel.uiState.collectAsStateWithLifecycle()
+
+    ObserveAsEvents(eventFlow) { adaptiveEvent ->
+        when (adaptiveEvent) {
+            is AdaptiveEvent.OnError -> { /* Handle error */ }
         }
     }
 
-    AdaptiveScreen(
+    AdaptiveContent(
         modifier = modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background),
-        movieDetail = uiState.movieDetail,
+        homeContent = {
+            HomeScreen(
+                uiState = homeUiState,
+                eventFlow = homeViewModel.eventFlow,
+                onRefresh = { homeViewModel.getConfig() }
+            )
+        },
+        detailContent = {
+            DetailScreen(
+                uiState = detailUiState,
+                eventFlow = detailViewModel.eventFlow,
+                onRefresh = { detailViewModel.getMovieDetails() },
+            )
+        }
     )
 }
 
+@OptIn(ExperimentalMaterial3AdaptiveApi::class)
 @Composable
 fun AdaptiveContent(
     modifier: Modifier = Modifier,
-    movieDetail: MovieDetail? = null
+    movieDetail: MovieDetail? = null,
+    onMovieClick: (String) -> Unit = {},
+    onBackClick: () -> Unit = {},
+    homeContent: @Composable () -> Unit = {},
+    detailContent: @Composable () -> Unit = {},
+    windowAdaptiveInfo: WindowAdaptiveInfo = currentWindowAdaptiveInfo(),
 ) {
-    var isFullscreen by rememberSaveable { mutableStateOf(false) }
+    val listDetailNavigator = rememberListDetailPaneScaffoldNavigator(
+        scaffoldDirective = calculatePaneScaffoldDirective(windowAdaptiveInfo),
+        initialDestinationHistory = listOfNotNull(
+            ThreePaneScaffoldDestinationItem(ListDetailPaneScaffoldRole.List),
+            ThreePaneScaffoldDestinationItem<Nothing>(ListDetailPaneScaffoldRole.Detail),
+        ),
+    )
+    BackHandler(listDetailNavigator.canNavigateBack()) {
+        onBackClick()
+    }
 
+    val homeScreenWidth = calculateHomeScreenWidth()
+    ListDetailPaneScaffold(
+        value = listDetailNavigator.scaffoldValue,
+        directive = listDetailNavigator.scaffoldDirective,
+        listPane = {
+            AnimatedPane(modifier = Modifier.preferredWidth(homeScreenWidth)) {
+                homeContent()
+            }
+        },
+        detailPane = {
+            AnimatedPane(modifier = Modifier) {
+                detailContent()
+            }
+        },
+    )
 
 }
 
-@PreviewUiMode
 @Composable
-private fun DetailScreenPreview() {
-    MovieCatalogTheme {
-        AdaptiveContent(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background),
-            movieDetail = MovieConstants.PLACEHOLDER_DETAILED_MOVIE
-        )
-    }
+private fun calculateHomeScreenWidth(): Dp {
+    val config = LocalConfiguration.current
+    val screenWidth = config.screenWidthDp.dp
+    return screenWidth * (0.6f)
 }
